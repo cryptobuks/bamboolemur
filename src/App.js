07294be8a065e70e957c8f1a041ea2e8f31e79ed
Vitalii Kravets <vitalii.kravets@gmail.com>
2018-06-23 10:07:43 +0200

    Add same host chat functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Create from './modules/create';
+import Join from './modules/join';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
@@ -10,9 +13,12 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+          <div>
+            <Route exact path="/" component={Create} />
+            <Route path="/join" component={Join} />
+          </div>
+        </Router>
       </div>
     );
   }
