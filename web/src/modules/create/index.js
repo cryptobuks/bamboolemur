5ce92e2f75ea6e9b23f343d767338e014be615e8
Vitalii Kravets <vitalii.kravets@gmail.com>
2018-07-03 13:46:37 +0200

    Add connect to topic

--- web/src/modules/create/index.js
+++ web/src/modules/create/index.js
@@ -2,8 +2,43 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { showText } from './actions';
 import { getText } from './selectors';
+import AWS from 'aws-sdk/global';
+import AWSMqtt from 'aws-mqtt';
+import config from '../../config';
 
 class Create extends Component {
+
+  componentDidMount(){
+    // generate random sessionId
+    //const topicName = makeId(); // SessionID
+
+    AWS.config.region = config.aws.region;
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: config.aws.cognito.identityPoolId
+    });
+    // create iot topic
+    const client = AWSMqtt.connect({
+      WebSocket: window.WebSocket,
+      region: AWS.config.region,
+      credentials: AWS.config.credentials,
+      endpoint: config.aws.iot.endpoint,
+      clientId: 'mqtt-client-' + (Math.floor((Math.random() * 100000) + 1))
+    });
+
+    // subsctibe to topic
+    client.on('connect', () => {
+      client.subscribe('/WillMsg');
+    });
+
+    client.on('message', (topic, message) => {
+      console.log(`${topic} => ${message}`);
+    });
+  }
+
+
+  // wait for other user to send {clientId: 123, message: JOINING}
+  // create offer for 123, send offer to topic
+  // wait for Participant's SDP, start chat
 
   render() {
     return (
@@ -17,6 +52,16 @@ class Create extends Component {
   };
 
 }
+
+// function makeId() {
+//   var text = "";
+//   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+//
+//   for (var i = 0; i < 5; i++)
+//     text += possible.charAt(Math.floor(Math.random() * possible.length));
+//
+//   return text;
+// }
 
 const mapStateToProps = state => ({
   text: getText(state)
--- web/src/modules/join/index.js
+++ web/src/modules/join/index.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 
 class Join extends Component {
 
-  constructor(props) {
-     super(props);
-     this.state = {}
-  }
-
   componentDidMount(){
     console.log(this.props.match.params.sessionId);
+    // connect to topic by sessionId
+    // generate clientId
+    // notify all by sending message {clientId: 123, message: JOINING}
+    // wait for message {clientId: 567, message: OFFER}
+    // CREATE Participant'S SDP
+    // send Participant'S SDP to 567
   }
 
   render() {
