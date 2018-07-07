8273077a72c4a9b00b0bef20e7925b8e7850798c
Vitalii Kravets <vitalii.kravets@gmail.com>
2018-07-07 12:00:07 +0200

    Fix tests.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { Map, List } from 'immutable';
+
+const initialState = Map({
+  create: Map({}),
+  join: Map({
+    messages: List(),
+    messageText: ''
+  })
+});
+const reducer = (state = initialState, action) => state;
+const testStore = createStore(reducer);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provider store={testStore}><App /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
